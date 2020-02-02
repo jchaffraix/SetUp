39016d65cd400a61d3262c70fd278b56ce8c8b87
Julien Chaffraix <julien.chaffraix@gmail.com>
2020-02-02 14:29:03 -0800

    Add the logic to clone the repository

--- install.py
+++ install.py
@@ -1,39 +1,48 @@
 import platform
 import subprocess
+import os
 
 # TODO: Add flags for these.
 # Configurable variables.
 # TODO: Windows?
-PATH="~/Tools"
+PATH=os.path.expanduser('~/Tools')
 DEBUG = True
 
 # Non-configurable variables.
-#REMOTE="https://github.com/jchaffraix/Tools.git
+REMOTE='git@github.com:jchaffraix/Tools.git'
 DEPS=['git', 'tmux', 'zsh']
 
 def _RunCommand(command):
     if DEBUG:
-      print "Running command %s" % command
+      print("Running command %s" % command)
     process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE)
     process.wait()
     return (process.returncode, process.stdout.read().strip())
 
 def install_software_deps():
-  os = platform.system()
-  if os == 'Linux':
+  print("Installing deps")
+  platform_os = platform.system()
+  if platform_os == 'Linux':
     # We only support Debian based packet managers.
-    print("Installing deps")
     _RunCommand(['sudo', 'apt-get', 'install'] + DEPS)
-  elif os == 'Darwin':
+  elif platform_os == 'Darwin':
     # Brew install the previous ones.
     pass
-  elif os == 'Windows':
+  elif platform_os == 'Windows':
     print("Can't install missing deps on Windows.")
   else:
-    print("Unknown OS %s" % os)
+    print("Unknown OS %s" % platform_os)
+
+def install_config():
+  print("Installing configs")
+  # Create the path and git clone into it.
+  os.makedirs(PATH, exist_ok=True)
+  _RunCommand(['git', 'clone', REMOTE, PATH])
+
 
 def install():
   install_software_deps()
+  install_config()
 
 if __name__ == '__main__':
   install()
